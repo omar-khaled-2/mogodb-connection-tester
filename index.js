9b3c6653e127fe7c78b06c4a83e71b0373f19fe1
Omar Abdal-salam <sphinx@192.168.1.2>
2024-05-12 11:26:23 +0300

    test

--- index.js
+++ index.js
@@ -10,50 +10,48 @@ const client = new MongoClient(url);
 
 console.log(url)
 
-// Database Name
+
 async function main() {
-  // Use connect method to connect to the server
+
   await client.connect();
   console.log('Connected successfully');
 
   const db = client.db("arabi-learn");
 
 
+console.log(await db.collection("skills").countDocuments())
+
+  //   const skills = JSON.parse(await fs.readFile("skills.json"))
+
+  //   for(const skill of skills) {
+  //       skill["_id"] = skill["_id"]["$oid"]
+  //       skill["_id"] = new ObjectId(skill["_id"])
+  //   }
+
+  //   await db.collection("skills").insertMany(skills)
 
 
-    const skills = JSON.parse(await fs.readFile("skills.json"))
+  //   const questions = JSON.parse(await fs.readFile("questions.json"))
 
-    for(const skill of skills) {
-        skill["_id"] = skill["_id"]["$oid"]
-        skill["_id"] = new ObjectId(skill["_id"])
-    }
+  //   for(const question of questions) {
+  //       question["_id"] = question["_id"]["$oid"]
+  //       question["_id"] = new ObjectId(question["_id"])
+  //       if(question["options"]) {
+  //           for(const option of question["options"]) {
+  //               option["_id"] = option["_id"]["$oid"]
+  //               option["_id"] = new ObjectId(option["_id"])
+  //           }
+  //       }
+  //   }
 
-    await db.collection("skills").insertMany(skills)
-
-
-    const questions = JSON.parse(await fs.readFile("questions.json"))
-
-    for(const question of questions) {
-        question["_id"] = question["_id"]["$oid"]
-        question["_id"] = new ObjectId(question["_id"])
-        if(question["options"]) {
-            for(const option of question["options"]) {
-                option["_id"] = option["_id"]["$oid"]
-                option["_id"] = new ObjectId(option["_id"])
-            }
-        }
-    }
-
-  await db.collection("questions").insertMany(questions)
-
-
-  
-
-  
+  // await db.collection("questions").insertMany(questions)
 
 
   
-console.log("migrated")
+
+  
+
+
 
   return 'done.';
 }
